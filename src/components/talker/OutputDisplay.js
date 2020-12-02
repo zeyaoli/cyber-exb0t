bcff9256733a051d29fac0bc5bbad3ec3daa6ead
zeyaoli <zeyao.li.315@gmail.com>
2020-12-02 11:33:31 -0500

    auto-scroll to bottom

--- src/components/talker/OutputDisplay.js
+++ src/components/talker/OutputDisplay.js
@@ -1,10 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ChatWrapper from "../talker/ChatWrapper";
 
 const OutputDisplay = ({ messages }) => {
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      });
+    }
+  }, [messages]);
   return (
     <div className={`container`} id='output'>
       <ChatWrapper messages={messages} />
+      <div ref={messagesEndRef} />
     </div>
   );
 };
